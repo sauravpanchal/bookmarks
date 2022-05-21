9f91e0d57c8be6da6d8333662ca4c5f7f9681637
sauravpanchal <saurav.panchal.08@gmail.com>
2022-05-21 22:18:03 +0530

    Generate Short URL

--- src/database.py
+++ src/database.py
@@ -3,6 +3,8 @@ from turtle import update
 from venv import create
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import string
+import random
 
 db = SQLAlchemy
 
@@ -24,13 +26,31 @@ class Bookmarks(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.Text, nullable = True)
     url = db.Column(db.Text, nullable = False)
-    short_url = db.Column(db.String(3), nullable = True)
+    short_url = db.Column(db.String(8), nullable = True)
     visits = db.Column(db.Integer, default = 0)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     created_at = db.Column(db.DateTime, default = datetime.now())
     updated_at = db.Column(db.DateTime, onupdate = datetime.now())
+
+    def generate_short_url(self):
+        char_choices = string.digits + string.ascii_letters
+        picked_chars = ''.join(random.choices(char_choices, k = 8))
+
+        link = self.query.filter_by(short_url = picked_chars).first()
+
+        # checks if short_url's combination exist in database or not.
+        # if it exists then generate again else return picked_chars
+        if link:
+            self.generate_short_url()
+        else:
+            return picked_chars
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.short_url = self.generate_short_url()
 
     def __repr__(self) -> str:
         return "Bookmark >> {self.url}"
