2d67ce5741cc70e3d0dea9a5dffb3ef7f96442e8
sauravpanchal <saurav.panchal.08@gmail.com>
2022-05-22 23:05:41 +0530

    created user login

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,5 @@
 export FLASK_ENV=development
 export FLASK_APP=src
 export SQLALCHEMY_DB_URI=sqlite:///bookmarks.db
+
+export JWT_SECRET_KEY = '8ICARUS8'
--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,7 @@ import os
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
 
 def create_app(test_config = None):
     app = Flask(__name__, instance_relative_config = True)
@@ -18,12 +19,16 @@ def create_app(test_config = None):
             SECRET_KEY = os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS = False,
+            JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
     
     db.app = app
     db.init_app(app)
+
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
